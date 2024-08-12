8c0af0992281eacbbab2f31220944f7b3d959ebf
efsunboz16 <devrim.boz16@gmail.com>
2024-08-12 19:54:56 +0300

    mailtrap verify code test

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from '../mailtrap/email.js'
 
 
 
@@ -33,7 +34,7 @@ export const signup = async (req, res) => {
         // jwt
         generateTokenAndSetCookie(res, user._id);
 
-        sendVerificationEmail(user.email, verificationToken)
+        await sendVerificationEmail(user.email, verificationToken)
 
         res.status(201).json({
             success: true,
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,21 +1,22 @@
-import { mailtrapClient, sender } from "./mailtrap.config.js"
+import { client, sender } from "./mailtrap.config.js"
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 
 
 export const sendVerificationEmail = async (email, verificationToken) => {
     const recipient = [{ email }]
 
     try {
-        const response = await mailtrapClient.send({
+        const response = await client.send({
             from: sender,
             to: recipient,
             subject: "Verify your email",
-
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+            category: "Email verification"
         })
+
+        console.log("Email sent successfully", response)
     } catch (error) {
-
+        console.log('Error sending verification', error);
+        throw new Error(`Error sending verification email: ${error}`);
     }
-
-
-
-
 }
