ee6cacca3dee89c070ab31c8903fcb5d1d07d21d
efsunboz16 <devrim.boz16@gmail.com>
2024-08-12 20:29:12 +0300

    sending welcome email

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -20,3 +20,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`);
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await client.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "7f85c4fa-7952-495b-b7e1-4d24599d76ae",
+            template_variables: {
+                company_info_name: "PUFFY & MUFFY",
+                name: name
+            }
+        });
+
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,15 +2,17 @@ import express from "express"
 import { signup } from "../controllers/auth.controller.js";
 import { login } from "../controllers/auth.controller.js";
 import { logout } from "../controllers/auth.controller.js";
+import { verifyEmail } from "../controllers/auth.controller.js";
+
 
 const router = express.Router();
 
 
 router.post("/signup", signup)
-
 router.post("/login", login)
-
 router.post("/logout", logout)
+
+router.post("/verify-email", verifyEmail)
 
 
 export default router;
