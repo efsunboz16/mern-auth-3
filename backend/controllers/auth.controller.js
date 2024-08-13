b17ef0447f1f200ec205ae2af79ce72d39169bba
efsunboz16 <devrim.boz16@gmail.com>
2024-08-13 18:49:17 +0300

    add login, logout and forgotpassword route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/email.js'
+import { sendVerificationEmail, sendWelcomeEmail, sendPasswordResetEmail, sendResetSuccessEmail } from '../mailtrap/email.js'
 
 
 
@@ -87,9 +88,100 @@ export const verifyEmail = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.send("login route")
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return req.status(400).json({ success: false, message: "Invalid credentials" })
+        }
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined
+            }
+        })
+
+    } catch (error) {
+        console.log("Error in login", error);
+        res.status(400).json({ success: false, message: error.message })
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("logout route")
+    res.clearCookie("token");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+}
+
+export const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return req.status(400).json({ success: false, message: "User not found" })
+        }
+
+        // Generate reset token 
+        const resetToken = crypto.randomBytes(20).toString("hex");
+        const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000 // 1 hour
+
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+        await user.save()
+
+        // send email
+        await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
+
+        res.status(200).json({ success: true, message: "Password reset link sent to your email" });
+    } catch (error) {
+        console.log("Error in forgotPassword", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const resetPassword = async (req, res) => {
+    try {
+        const { token } = req.params;
+        const { password } = req.body;
+
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: { $gt: Date.now() }
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired reset token" })
+        }
+
+
+        // update password
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpiresAt = undefined;
+        await user.save();
+
+        await sendResetSuccessEmail(user.email);
+
+        res.status(200).json({ success: true, message: "Password reset successfully" });
+
+    } catch (error) {
+        console.log("Error in resetPassword ", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup } from "../controllers/auth.controller.js";
+import { forgotPassword, signup, resetPassword } from "../controllers/auth.controller.js";
 import { login } from "../controllers/auth.controller.js";
 import { logout } from "../controllers/auth.controller.js";
 import { verifyEmail } from "../controllers/auth.controller.js";
@@ -13,6 +13,9 @@ router.post("/login", login)
 router.post("/logout", logout)
 
 router.post("/verify-email", verifyEmail)
+router.post("/forgot-password", forgotPassword)
+
+router.post("/reset-password/:token", resetPassword)
 
 
 export default router;
