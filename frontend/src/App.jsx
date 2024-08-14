76f45d4d9d50e4b8a1f1f487c7794151096bf90e
efsunboz16 <devrim.boz16@gmail.com>
2024-08-14 15:08:53 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-green-200 flex justify-center items-center relative overflow-hidden">
+      <h1 className="text-red-400">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
